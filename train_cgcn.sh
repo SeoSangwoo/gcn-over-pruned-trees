dc8e4ce92881c165f55142d658f678d5a1c995af
root <johnary@naver.com>
2019-11-14 08:50:36 +0000

    semeval

--- train_cgcn.sh
+++ train_cgcn.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 SAVE_ID=$1
-python train.py --id $SAVE_ID --seed 0 --prune_k 1 --lr 0.5 --lr_decay 0.95 --rnn_hidden 100 --num_epoch 500 --pooling max --mlp_layers 2 --pooling_l2 0.003 --data_dir dataset/semeval
+python train.py --id $SAVE_ID --seed 0 --prune_k 1 --lr 0.7 --lr_decay 0.95 --rnn_hidden 100 --num_epoch 500 --pooling max --mlp_layers 1 --pooling_l2 0.004 --data_dir dataset/semeval
